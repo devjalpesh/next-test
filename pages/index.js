efcf2aa3cf509dd54252b25b14d03f2ac4a73894
Jalpesh Patel <jalpesh@integralconsult.co.uk>
2022-12-21 02:10:03 +0530

    minor change

--- pages/index.js
+++ pages/index.js
@@ -54,7 +54,7 @@ export default function Home() {
               Docs <span>-&gt;</span>
             </h2>
             <p className={inter.className}>
-              Find in-depth information about Next.js features and&nbsp;API.
+              Find .
             </p>
           </a>
 
